8095d7624a7d5fc5489b2f95a17b50cab2a6320f
=bibin thomas <bibinthomas951@gmail.com>
2022-10-24 19:17:48 +0530

    Code refactored

--- test.py
+++ test.py
@@ -46,7 +46,7 @@ def doctors():
     """
 
     print("Hey!! Doc nice to see you !!")
-    print(colors.OKBLUE+"What you want to do today ? "+colors.ENDC)
+    print(f"{colors.OKBLUE}What you want to do today ? {colors.ENDC}")
     sleep(0.8)
     print("""
     1.view patients name and their personal data 
@@ -59,19 +59,17 @@ def doctors():
         if a == 1:
             pname = input("Enter the name of the patient:").lower().strip()
 
-            value = patdb.find({"name":pname}) # retrieving the data from the database by the [name]
-            if value:
+            if value := patdb.find({"name": pname}):
                 print(" \n name :{0}\n age: {1} \nblood group : {2}\n address : {3}\n".format(value[0]["name"], value[0]["age"], value[0]["bloodGRP"], value[0]["address"]))
 
             else:
                 print(colors.WARNING+"The patient doesn't exitst !!"+colors.ENDC)
 
-        elif a == 2 :
+        elif a == 2:
             bloodGroup = input("Enter the blood group: ").upper()
             print(bloodGroup)
             query = bloodb.find({"bloodgrp":bloodGroup})
-            query=list(query)
-            if query:
+            if query := list(query):
                 print("The blood group {0} exits in blood bank cout: {1} ".format(query[0]["bloodgrp"],query[0]["count"]))
             else:
                 print("The blood group doesn't exists in the bank!!!")
@@ -80,13 +78,13 @@ def doctors():
             dname = str(input("Enter the name :"))
             role = str(input("Enter the role:"))
             docdb.insert({"name":dname,"splt":role})
-            print(colors.OKGREEN+"Data added"+colors.ENDC)
-        
-        elif a == 4: 
+            print(f"{colors.OKGREEN}Data added{colors.ENDC}")
+
+        elif a == 4:
             data = list(docdb.find({}))
-            for i in range(len(data)):
-                print("name:{0} Role:{1}".format(data[i]["name"],data[i]["splt"]))
-        else : 
+            for datum in data:
+                print("name:{0} Role:{1}".format(datum["name"], datum["splt"]))
+        else:
             raise ValueError
     except ValueError :
         print("Check the option you have entered!!!!")
@@ -102,90 +100,91 @@ def patient():
     option = int(input())
     year = datetime.date.today().year
     today =str(datetime.date.today())
-
-
-
-    if option == 1 :
+    if option == 1:
         try:
-            name = str(input("Please Enter your name: ")).lower()
-            age = int(input("please Enter your age :"))
-            birth_year = int (input("Please Enter year: "))
-            add = str(input("Enter the present address:"))
-            bloodgrp = input("Enter your blood group:").upper()
-            
-            
-            if (year-birth_year)>=18 and year-birth_year == age :
-                try: 
-                    insert = {
-                        "name":name,
-                        "age":age,
-                        "birth_year":birth_year,
-                        "address":add,
-                        "bloodGRP":bloodgrp,
-                        "date":today        
-                                }
-                    patdb.insert_one(insert)
-                    if patdb:
-                        print(colors.OKGREEN+"YOURE DATA IS ADDED SUCCESSFULLY!!!"+colors.ENDC)
-                        update() # calls thee function to upodate the user data into the blood document 
-                    else:
-                        raise custom
-                except errors.DuplicateKeyError:
-                    print(colors.WARNING+"The user is already present inside the database !!!!!"+colors.ENDC)
-            else: 
-                print(colors.BOLD+"You are too young to give blood !!!!"+colors.ENDC)
+            _extracted_from_patient_12(year, today)
         except custom:
-            print(colors.FAIL+"ERROR WITH ADDING DATA OR DATABASE ERROR !!!!"+colors.ENDC)
+            print(f"{colors.FAIL}ERROR WITH ADDING DATA OR DATABASE ERROR !!!!{colors.ENDC}")
+
 
         except KeyboardInterrupt:
-            print(colors.FAIL+"Terminating......"+colors.ENDC)
-            
+            print(f"{colors.FAIL}Terminating......{colors.ENDC}")
+
     elif option == 2:
         check_db = str(input("please enter the name of the patient: ")).lower()
-        data_check =list( patdb.find({"name":check_db}))
-
-        if data_check:
+        if data_check := list(patdb.find({"name": check_db})):
             print("name :{0}\n age: {1} \nblood group : {2}\n address : {3}\n".format(data_check[0]["name"], data_check[0]["age"], data_check[0]["bloodGRP"], data_check[0]["address"]))
         else:
             print("No details found regarding this user !!!")
 
     elif option == 3:
-        print("Please enter the updated values::: ")
-        name = str(input("Enter who's do u want to change:")).lower()
-        update_name = str(input("Enter name : ")).lower()
-        age = int(input("please Enter your age :"))
-        birth_year = int (input("Please Enter year: "))
-        add = str(input("Enter the present address:"))
-        bloodgrp = input("Enter your blood group:").upper()
-
-        updated = patdb.replace_one({"name":name},{"name":update_name,"age":age,"birth_year":birth_year,"address":add,"bloodGRP":bloodgrp,"date":today})
-        if updated : 
-            print("The user has been found and the changes have been made !!")
-        else :
-            print("User not found !!!")
-        
-    else :
+        _extracted_from_patient_(today)
+    else:
         raise ValueError
+
+
+# TODO Rename this here and in `patient`
+def _extracted_from_patient_(today):
+    print("Please enter the updated values::: ")
+    name = str(input("Enter who's do u want to change:")).lower()
+    update_name = str(input("Enter name : ")).lower()
+    age = int(input("please Enter your age :"))
+    birth_year = int (input("Please Enter year: "))
+    add = str(input("Enter the present address:"))
+    bloodgrp = input("Enter your blood group:").upper()
+
+    if updated := patdb.replace_one({"name": name}, {"name": update_name, "age": age, "birth_year": birth_year, "address": add, "bloodGRP": bloodgrp, "date": today}):
+        print("The user has been found and the changes have been made !!")
+    else:
+        print("User not found !!!")
+
+
+# TODO Rename this here and in `patient`
+def _extracted_from_patient_12(year, today):
+    name = str(input("Please Enter your name: ")).lower()
+    age = int(input("please Enter your age :"))
+    birth_year = int (input("Please Enter year: "))
+    add = str(input("Enter the present address:"))
+    bloodgrp = input("Enter your blood group:").upper()
+
+
+    if (year-birth_year)>=18 and year-birth_year == age:
+        try: 
+            insert = {
+                "name":name,
+                "age":age,
+                "birth_year":birth_year,
+                "address":add,
+                "bloodGRP":bloodgrp,
+                "date":today        
+                        }
+            patdb.insert_one(insert)
+            if not patdb:
+                raise custom
+            print(f"{colors.OKGREEN}YOURE DATA IS ADDED SUCCESSFULLY!!!{colors.ENDC}")
+            update() # calls thee function to upodate the user data into the blood document 
+        except errors.DuplicateKeyError:
+            print(f"{colors.WARNING}The user is already present inside the database !!!!!{colors.ENDC}")
+
+    else: 
+        print(f"{colors.BOLD}You are too young to give blood !!!!{colors.ENDC}")
 
 def bloodBank():
     print("Welcome to blood bank !!!")
     blood = str(input("Enter the blood group:")).upper()
     bloodlist = ["A+","A-","AB+","AB-","B+","B-","O+","O-"]
     query_blood = list(bloodb.find({"bloodgrp":blood}))
-    if blood in bloodlist:        
-        print("the amount of the blood available is {} packs".format(query_blood[0]["count"]))
+    if blood in bloodlist:    
+        print(f'the amount of the blood available is {query_blood[0]["count"]} packs')
     else:
-        print(colors.FAIL+"Please re-enter the blood group!!! "+colors.ENDC)
+        print(f"{colors.FAIL}Please re-enter the blood group!!! {colors.ENDC}")
     
 def update():
     pdb = list(patdb.find())
-    count_bloodGRP = []
+    count_bloodGRP = [item["bloodGRP"] for item in pdb]
 
-    for i in range(len(pdb)):
-        count_bloodGRP.append(pdb[i]["bloodGRP"])
-
-    for i in range(len(count_bloodGRP)):
-        bloodb.update({"bloodgrp":count_bloodGRP[i]},{"$inc":{"count":1}})
+    for item in count_bloodGRP:
+        bloodb.update({"bloodgrp": item}, {"$inc":{"count":1}})
             
 
         
@@ -200,7 +199,7 @@ print("""
 1.Doctor
 2.patient
 3.require blood """)
-try :    
+try:    
     option = int(input())
 
     if option == 1 : 
@@ -212,7 +211,7 @@ try :
     else:
         raise ValueError
 
-except KeyboardInterrupt :
-    print(colors.FAIL+"Terminating......"+colors.ENDC)
+except KeyboardInterrupt:
+    print(f"{colors.FAIL}Terminating......{colors.ENDC}")
 
 
